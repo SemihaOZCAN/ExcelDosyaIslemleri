f58c3de7132b663aca9c1b810c58dd84fa2486c4
Semiha OZCAN <169284658+SemihaOZCAN@users.noreply.github.com>
2024-09-09 23:59:11 +0300

    ExcelDosyaIslemleri

---
+++ ExcelIslem/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ExcelIslem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonListele = new System.Windows.Forms.Button();
+            this.buttonKaydet = new System.Windows.Forms.Button();
+            this.textBoxTarih = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxSaat = new System.Windows.Forms.TextBox();
+            this.textBoxDers = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 4);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(604, 295);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonListele
+            // 
+            this.buttonListele.Location = new System.Drawing.Point(626, 13);
+            this.buttonListele.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonListele.Name = "buttonListele";
+            this.buttonListele.Size = new System.Drawing.Size(279, 34);
+            this.buttonListele.TabIndex = 1;
+            this.buttonListele.Text = "Listele";
+            this.buttonListele.UseVisualStyleBackColor = true;
+            this.buttonListele.Click += new System.EventHandler(this.buttonListele_Click);
+            // 
+            // buttonKaydet
+            // 
+            this.buttonKaydet.Location = new System.Drawing.Point(626, 67);
+            this.buttonKaydet.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonKaydet.Name = "buttonKaydet";
+            this.buttonKaydet.Size = new System.Drawing.Size(279, 34);
+            this.buttonKaydet.TabIndex = 2;
+            this.buttonKaydet.Text = "Kaydet";
+            this.buttonKaydet.UseVisualStyleBackColor = true;
+            this.buttonKaydet.Click += new System.EventHandler(this.buttonKaydet_Click);
+            // 
+            // textBoxTarih
+            // 
+            this.textBoxTarih.Location = new System.Drawing.Point(692, 132);
+            this.textBoxTarih.Name = "textBoxTarih";
+            this.textBoxTarih.Size = new System.Drawing.Size(190, 30);
+            this.textBoxTarih.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(622, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tarih:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(622, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Saat:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(622, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ders:";
+            // 
+            // textBoxSaat
+            // 
+            this.textBoxSaat.Location = new System.Drawing.Point(692, 177);
+            this.textBoxSaat.Name = "textBoxSaat";
+            this.textBoxSaat.Size = new System.Drawing.Size(190, 30);
+            this.textBoxSaat.TabIndex = 7;
+            // 
+            // textBoxDers
+            // 
+            this.textBoxDers.Location = new System.Drawing.Point(692, 222);
+            this.textBoxDers.Name = "textBoxDers";
+            this.textBoxDers.Size = new System.Drawing.Size(190, 30);
+            this.textBoxDers.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 309);
+            this.Controls.Add(this.textBoxDers);
+            this.Controls.Add(this.textBoxSaat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTarih);
+            this.Controls.Add(this.buttonKaydet);
+            this.Controls.Add(this.buttonListele);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonListele;
+        private System.Windows.Forms.Button buttonKaydet;
+        private System.Windows.Forms.TextBox textBoxTarih;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxSaat;
+        private System.Windows.Forms.TextBox textBoxDers;
+    }
+}
+
---
+++ ExcelIslem/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace ExcelIslem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection baglanti = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Semiha\Desktop\Bilgisayar_Muhendisligi_Ders_Programi.xlsx;Extended Properties='Excel 12.0 Xml;HDR=YES;'");
+
+        void listele()
+        {
+            try
+            {
+                baglanti.Open();
+                OleDbDataAdapter da = new OleDbDataAdapter("SELECT * FROM [Sheet1$]", baglanti);
+                DataTable dataTable = new DataTable();
+                da.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (OleDbException ex)
+            {
+                MessageBox.Show("Veritabanı hatası: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Genel hata: " + ex.Message);
+            }
+            finally
+            {
+                if (baglanti.State == ConnectionState.Open)
+                {
+                    baglanti.Close();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Form yüklendiğinde yapılacak işlemler
+        }
+
+        private void buttonListele_Click(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void buttonKaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                OleDbCommand komut = new OleDbCommand("INSERT INTO [Sheet1$] (Tarih, Saat, [Ders Başlığı]) VALUES (?, ?, ?)", baglanti);
+                komut.Parameters.AddWithValue("?", textBoxTarih.Text);
+                komut.Parameters.AddWithValue("?", textBoxSaat.Text);
+                komut.Parameters.AddWithValue("?", textBoxDers.Text);
+                komut.ExecuteNonQuery();
+                MessageBox.Show("Yeni ders bilgisi eklendi");
+                listele();
+            }
+            catch (OleDbException ex)
+            {
+                MessageBox.Show("baglantı hatası: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Genel hata: " + ex.Message);
+            }
+            finally
+            {
+                if (baglanti.State == ConnectionState.Open)
+                {
+                    baglanti.Close();
+                }
+            }
+        }
+    }
+}
